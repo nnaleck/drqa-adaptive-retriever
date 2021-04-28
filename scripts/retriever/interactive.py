4b83afd96d65362c574d1160fdb4782df6e2f9b2
nnaleck <ilyes_faicel.kamel@g.enp.edu.dz>
2021-04-28 15:01:31 +0200

    wip

--- scripts/retriever/interactive.py
+++ scripts/retriever/interactive.py
@@ -11,6 +11,7 @@ import code
 import prettytable
 import logging
 from drqa import retriever
+import pandas as pd
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -21,10 +22,17 @@ logger.addHandler(console)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default=None)
+parser.add_argument('--csv', type=str, default=None)
 args = parser.parse_args()
 
 logger.info('Initializing ranker...')
 ranker = retriever.get_class('tfidf')(tfidf_path=args.model)
+
+# Loading questions
+if args.csv:
+    df = pd.read_csv(args.csv)
+    df = df[df["Full_Text"].notna()]
+    df = df.reset_index()
 
 
 # ------------------------------------------------------------------------------
@@ -40,6 +48,25 @@ def process(query, k=1):
     for i in range(len(doc_names)):
         table.add_row([i + 1, doc_names[i], '%.5g' % doc_scores[i]])
     print(table)
+
+
+def calculate_top_n_ratio(top_n=3):
+    nb_included_in_top_n = 0
+    positions_sum = 0
+
+    for i in df.index:
+        question_query = df["Question"][i]
+        pmid_query = str(df["Document_ID"][i])
+
+        doc_names, doc_scores = ranker.closest_docs(question_query, top_n)
+
+        for j in range(len(doc_names)):
+            pmid_result = doc_names[j]
+            if pmid_result == pmid_query:
+                nb_included_in_top_n += 1
+                positions_sum += (j + 1)
+
+    return nb_included_in_top_n, nb_included_in_top_n / df.shape[0], positions_sum / nb_included_in_top_n
 
 
 banner = """
